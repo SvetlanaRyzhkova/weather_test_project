d4900f6c7e4674f53dd08a72db8b34d012514c6a
Svetlana Ryzhkova <sveta.ryzhkova.00@bk.ru>
2023-02-27 15:52:08 +0400

    добавила запрос погоды на определённое количество дней, добавила вспомогательную информацию

--- weather_project/Program.cs
+++ weather_project/Program.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace weather_test_project
 {
@@ -37,15 +38,30 @@ namespace weather_test_project
                 }
                 else if (needHelp || answear == "-h" || answear == "--help")
                 {
-                    Console.WriteLine("Help information");
+                    Console.WriteLine("Help information\nWrite the name of the city and forecast for how many days you need including today.");
+                    Console.WriteLine("Examples:\nmoscow -7d\t - 7 days forecast\nmoscow -d\t - today's forecast");
+                    Console.WriteLine("If you want to close the program write -q or --quit.");
                     needHelp = false;
                 }
                 else
                 {
-                    HttpClient client = new HttpClient();
+                    //получаем город и количество дней для запроса
+                    var answearParts = answear.Split(' ');
+                    string city = answearParts[0];
+                    int numberOfDays = 0;
+                    try
+                    {
+                        numberOfDays = answearParts[1] == "-d" ? 0 : Convert.ToInt32(Regex.Matches(answearParts[1], @"^.*(?=d)")[0].Value.Substring(1));
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Couldn't understand your request. Read the help information.\nTo read help information send -h or --help.");
+                        continue;
+                    }
 
+                    HttpClient client = new HttpClient();                    
                     // получаем координаты населённого пункта
-                    HttpResponseMessage coordinatesResponse = await client.GetAsync($"https://geocoding-api.open-meteo.com/v1/search?name={answear}");
+                    HttpResponseMessage coordinatesResponse = await client.GetAsync($"https://geocoding-api.open-meteo.com/v1/search?name={city}");
                     if (!coordinatesResponse.IsSuccessStatusCode){
                         continue;
                     }
@@ -60,8 +76,11 @@ namespace weather_test_project
                     string latitude = results[0].GetProperty("latitude").ToString(); 
                     string longitude = results[0].GetProperty("longitude").ToString();
 
+                    string startDate = DateTime.UtcNow.ToString("yyyy-MM-dd");
+                    string endDate = DateTime.UtcNow.AddDays(numberOfDays).ToString("yyyy-MM-dd");
+
                     //получаем погодные значения
-                    HttpResponseMessage forecastResponse = await client.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m");
+                    HttpResponseMessage forecastResponse = await client.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&start_date={startDate}&end_date={endDate}");
                     if (!forecastResponse.IsSuccessStatusCode)
                     {
                         Console.WriteLine("Couldn't receive data. Try again!");
@@ -78,7 +97,7 @@ namespace weather_test_project
                     }
                     ForecastOfTheCity forecast = new ForecastOfTheCity
                     {
-                        city = answear,
+                        city = city,
                         latitude = latitude,
                         longitude = longitude,
                         forecastForAnHours = forecastForAnHours
